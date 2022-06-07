f14747cf97e1a6807fb054eeb8b68025770d067d
Jeff Halter <868228+jhalter@users.noreply.github.com>
2022-06-07 16:37:30 -0700

    Update mobius-hotline-client.rb

--- mobius-hotline-client.rb
+++ mobius-hotline-client.rb
@@ -1,7 +1,7 @@
 class MobiusHotlineClient < Formula
   desc "CLI Hotline client"
   homepage "https://github.com/jhalter/mobius"
-  url "https://github.com/jhalter/mobius/archive/refs/tags/v0.5.0.tar.gz"
+  url "https://github.com/jhalter/mobius/archive/refs/tags/v0.5.1.tar.gz"
   sha256 "f5a8ecdc3a6edb8f4178861cf8d1c88ffba289fb3c5c06fd22f8af5efc1f7a32"
   license "MIT"
 
