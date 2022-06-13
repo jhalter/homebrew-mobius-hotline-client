90ab919a7066417e0ac8485d41c5b76a8ea4c6db
Jeff Halter <868228+jhalter@users.noreply.github.com>
2022-06-12 21:17:08 -0700

    Update mobius-hotline-client.rb

--- mobius-hotline-client.rb
+++ mobius-hotline-client.rb
@@ -2,7 +2,7 @@ class MobiusHotlineClient < Formula
   desc "CLI Hotline client"
   homepage "https://github.com/jhalter/mobius"
   url "https://github.com/jhalter/mobius/archive/refs/tags/v0.5.6.tar.gz"
-  sha256 "67a197c7acb0bac60a43b84ce3b199a17cfd59c780a6598e26e9649b637ef599"
+  sha256 "6cc4741c2802e490a7424ab2d7a19011057e68b222e4ae458245cca568a7f399"
   license "MIT"
 
   depends_on "go" => :build
@@ -10,7 +10,7 @@ class MobiusHotlineClient < Formula
   def install
     system "make", "build-darwin-amd64-client"
     bin.install "dist/mobius_client_darwin_amd64/mobius-hotline-client" => "mobius-hotline-client"
-    etc.install "cmd/mobius-hotline-client/mobius-client-config.yaml"  => "mobius-client-config.yaml"
+    etc.install "cmd/mobius-hotline-client/mobius-client-config.yaml"  => "mobius-client-config.yaml" unless File.exist? etc/"mobius-client-config.yaml"
   end
 
   test do
