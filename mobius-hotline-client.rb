21ec00c3cbfd6d4c8534f0ab00fa4c4b1542b68e
Jeff Halter <868228+jhalter@users.noreply.github.com>
2025-12-05 13:04:26 -0800

    Update URL to version 0.2.4
    Updated the URL to the latest version 0.2.4.

--- mobius-hotline-client.rb
+++ mobius-hotline-client.rb
@@ -1,7 +1,7 @@
 class MobiusHotlineClient < Formula
   desc "CLI Hotline client"
   homepage "https://github.com/jhalter/mobius-hotline-client"
-  url "https://github.com/jhalter/mobius-hotline-client/archive/refs/tags/v0.2.0.tar.gz"
+  url "https://github.com/jhalter/mobius-hotline-client/archive/refs/tags/v0.2.4.tar.gz"
   sha256 "0c48440022479d11edfbc02a46a5bfb3db554839c9196ec4dcd250787fe651ff"
   license "MIT"
 
